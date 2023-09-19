606ab6d32afebb3fef4bb8b51b2f8d991cbd5f44
Abbeythetechguy <abbeyezeilo323@gmail.com>
2023-09-19 03:13:24 +0100

    Modified changes

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let showMenu = false;
 function showHamb() {
   if (!showMenu) {
     hambBtn.classList.add("open");
+    hambBtn.classList.add("open-fixed");
     mobileNav.classList.add("open")
 
     showMenu = true;
   } else {
     hambBtn.classList.remove("open");
+    hambBtn.classList.remove("open-fixed");
     mobileNav.classList.remove("open")
 
     showMenu = false;
